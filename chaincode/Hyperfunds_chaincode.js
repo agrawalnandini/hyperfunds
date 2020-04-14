066020b8196c78952f85211c6e31e152b8bc267e
Prashanthi Ramachandran <prashirams@gmail.com>
2020-04-14 18:39:15 +0530

    Add initledger and getbalance to updated master

--- chaincode/Hyperfunds_chaincode.js
+++ chaincode/Hyperfunds_chaincode.js
@@ -6,11 +6,53 @@ const ClientIdentity = require('fabric-shim').ClientIdentity;       //retrieving
 var balance={};     //global dictionary for balance with key:faculty_email and value: account balance
 let txnID=-1;
 let threshold=40000;    //look into it later! Maybe let the user decide interactively
+let dor_email = "dor@ashoka.edu.in";
+let accdept_email = "accounts@ashoka.edu.in";
 
 class Hyperfunds extends Contract
 {
     async initLedger(ctx){
+    console.info('============= START : Initialize Ledger ===========');
 
+        const startKey = '0';
+        const endKey = '99999';
+
+        const iterator = await ctx.stub.getStateByRange(startKey, endKey); // create an iterator to go over all transactions in the blockchain
+
+        while (true) {
+            const res = await iterator.next();
+
+            if (res.value && res.value.value.toString()) {
+                // console.log(res.value.value.toString('utf8'));
+                let prop; // proposal object
+                try {
+                    prop = JSON.parse(res.value.value.toString('utf8')); //convert the value to a JSON object
+
+                    // update balance array and txnID
+                    if (prop.amount === "0") { // if proposal amount is 0, update the balance to be 0 for that faculty member.
+                        balance[prop.faculty_email_id] = prop.proposed_amount;
+                    }
+                    else { // if proposal amount is not 0, add that to the balance for that faculty member
+                        balance[prop.faculty_email_id] = prop.balance[faculty_email_id] + prop.proposed_amount;
+                    }
+                
+                    txnID += 1; //increment txnID as we iterate through transactions
+
+                } catch (err) {
+                    console.log(err);
+                    prop = res.value.value.toString('utf8');
+                }
+            }
+
+            if (res.done) {
+                await iterator.close();
+                console.log(`Balance array: ${balance}`);
+                console.log(`num of faculty members registered: ${balance.length}`);
+                console.log(`Last txn ID: ${txnID}`);
+                break;
+            }
+        }
+        console.info('============= END : Initialize Ledger ===========');
     }
 
     async CreateProposalTxn(ctx,proposed_amount,faculty_email_id){
@@ -118,8 +160,17 @@ class Hyperfunds extends Contract
     }
 
     async getbalance(){
+    //retrieving the id of the approver: note we are defining the ids
+    let cid = new ClientIdentity(ctx.stub);
+    let approverid = cid.getID();
 
+    //approval cannot come from faculty
+    if((approverid.includes(dor_email)||approverid.includes(accdept_email)||(approverid.includes(faculty_email_id)))==true) {
+        return balance[faculty_email_id];
+    }     
+    else {
+        throw new Error(`Unauthorised access to Balance detected`);
     }
-
+    }
 
 }
