cfc79a114f5bab9330d2ce48cbda86769ca98cf1
agrawalnandini <nandini.agrawal03@gmail.com>
2020-04-18 20:59:00 +0530

    changed network stuff

---
+++ fabric-samples/chaincode/hyperfunds/javascript/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const Hyperfunds = require('./lib/hyperfunds');
+
+module.exports.Hyperfunds = Hyperfunds;
+module.exports.contracts = [ Hyperfunds ];
