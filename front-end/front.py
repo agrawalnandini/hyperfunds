7d62c77aa2c512188814f3cf7ee98077a35111bb
agrawalnandini <nandini.agrawal03@gmail.com>
2020-04-27 17:11:42 +0530

    updated html files for flask

--- front-end/front.py
+++ front-end/front.py
@@ -16,5 +16,34 @@ def signup():
 def dor_home():
     return render_template('%s.html' % 'dor_home')
 
+@app.route('/faculty_home.html')
+def faculty_home():
+    return render_template('%s.html' % 'faculty_home')
+
+@app.route('/accounts_home.html')
+def accounts_home():
+    return render_template('%s.html' % 'accounts_home')
+
+@app.route('/Proposal.html')
+def Proposal():
+    return render_template('%s.html' % 'Proposal')
+
+@app.route('/Approval.html')
+def Approval():
+    return render_template('%s.html' % 'Approval')
+
+@app.route('/getbalance.html')
+def getbalance_admin():
+    return render_template('%s.html' % 'getbalance_admin')
+
+@app.route('/query_email.html')
+def query_email():
+    return render_template('%s.html' % 'query_email')
+
+@app.route('/query.html')
+def query():
+    return render_template('%s.html' % 'query')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
