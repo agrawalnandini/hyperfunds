2c067286d7fa893479b4543255f24dfa0511ca38
agrawalnandini <nandini.agrawal03@gmail.com>
2020-05-02 15:05:11 +0530

    some changes in app.py and html files

--- fabric-samples/hyperfunds/teardownHyperfunds.sh
+++ fabric-samples/hyperfunds/teardownHyperfunds.sh
@@ -3,5 +3,8 @@ rm -r wallet
 cd ../../basic-network
 ./teardown.sh
 
+cd ../../front-end
+rm -rf db.json
+
 # rm -r /home/r/PycharmProjects/fabchat_flask_server/wallet
 # rm -r /home/r/PycharmProjects/fabchat_flask_server/client_db.json
